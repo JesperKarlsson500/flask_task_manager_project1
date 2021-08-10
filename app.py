d745982179a361c4c76c467df0e496a70c56d405
JesperKarlsson500 <jesper.karlsson500@gmail.com>
2021-08-10 20:40:13 +0000

    Add register template

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ the env.py file itself.
 """
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -28,10 +29,16 @@ mongo = PyMongo(app)
 @app.route("/")
 @app.route("/get_tasks")
 def get_tasks():
-    tasks = mongo.db.tasks.find() # latest tasks = this
+    tasks = mongo.db.tasks.find()
+    # latest tasks = this
     return render_template("tasks.html", tasks=tasks)
     # This will find all documents from the
     # tasks collection, and assign them to our new 'tasks'
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
 
 
 if __name__ == "__main__":
