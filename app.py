2e0577050bfa0d0fbf36d297b4dc854b07d256a4
JesperKarlsson500 <jesper.karlsson500@gmail.com>
2021-08-10 10:28:00 +0000

    create base template

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
-from flask import Flask
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
 """
 Since we are not going to push the env.py file to GitHub,
 once our app is deployed to Heroku, it won't be able
@@ -8,15 +10,28 @@ This is why we need to only import env if the os
 can find an existing file path for
 the env.py file itself.
 """
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
+
+
 @app.route("/")
-def Hello():
-    return "Hello World... Again!"
+@app.route("/get_tasks")
+def get_tasks():
+    tasks = mongo.db.tasks.find() # latest tasks = this
+    return render_template("tasks.html", tasks=tasks)
+    # This will find all documents from the
+    # tasks collection, and assign them to our new 'tasks'
 
 
 if __name__ == "__main__":
