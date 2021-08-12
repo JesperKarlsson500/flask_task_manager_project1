c3c428b223e193a5d34583a3536280a0e9c22d1e
JesperKarlsson500 <jesper.karlsson500@gmail.com>
2021-08-12 23:35:05 +0000

    add session user profile

--- app.py
+++ app.py
@@ -71,18 +71,18 @@ def login():
         # check if username exists in db
         existing_user = mongo.db.users.find_one(
             {"username": request.form.get("username").lower()})
-        
+
         if existing_user:
             # ensure hashed password matches user input.
             # to target that user's password in the database,
             # we append the key that we need inside of square-brackets.
             if check_password_hash(
-                existing_user["password"], request.form.get("password")):
-                    session["user"] = request.form.get("username").lower()
-                    flash("Welcome, {}".format(
-                        request.form.get("username")))
-                    return redirect(url_for(
-                        "profile", username=session["user"]))
+                    existing_user["password"], request.form.get("password")):
+                        session["user"] = request.form.get("username").lower()
+                        flash("Welcome, {}".format(
+                            request.form.get("username")))
+                        return redirect(url_for(
+                            "profile", username=session["user"]))
             else:
                 # Invalid password match
                 flash("Incorrect Username and/or Password")
@@ -92,16 +92,20 @@ def login():
             # username doesn't exist
             flash("Incorrect Username and/or Password")
             return redirect(url_for("login"))
-            
+
     return render_template("login.html")
 
 
 @app.route("/profile/<username>", methods=["GET", "POST"])
 def profile(username):
     # grab the sessions user's username from db
+    # The first 'username' is what the template
+    # is expecting to retrieve on the HTML file.
+    # The second 'username' is what we've defined
+    # on the line above.
     username = mongo.db.users.find_one(
         {"username": session["user"]})["username"]
-    return render_template("prifle.html", username=username)
+    return render_template("profile.html", username=username)
 
 
 if __name__ == "__main__":
